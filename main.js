9eca5976023b120f3af1d93bafaf8695b15461f9
AzureSoda <tae06061@naver.com>
2023-01-23 23:55:26 +0900

    [Add] +버튼 누르면 열고 닫는 기능 추가

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let inputBox = document.getElementById('inputField'); // 할 일 입력창
-let addToDo = document.getElementById('addToDo'); // 버튼
+let add = document.getElementById('add'); // 버튼
 let toDoList = document.getElementById('toDoList'); // 할 일 리스트창
+let plus = document.getElementById('plus');
+let newSchedule = document.getElementById('newSchedule');
 
-addToDo.addEventListener('click', function() { // 버튼에 클릭 이벤트가 발생하면
+
+add.addEventListener('click', function() { // 버튼에 클릭 이벤트가 발생하면
+    console.log("전효정");
+    /*
     var list = document.createElement('li'); // html 'li' 태그 만들기
     if (!inputBox.value) // 할 일 입력창에 내용이 입력되지 않으면 alert 발생
         alert('내용을 입력해 주세요!');
@@ -18,4 +23,17 @@ addToDo.addEventListener('click', function() { // 버튼에 클릭 이벤트가 
     list.addEventListener('dblclick', function() { // list에 더블클릭 이벤트가 발생하면 할 일 리스트창에서 지우기
         toDoList.removeChild(list);
     })
+    */
+})
+
+
+
+plus.addEventListener('click', function() {
+
+    var newSchedule = document.getElementById("newSchedule");
+    if (newSchedule.style.display == 'none') {
+        newSchedule.style.display = 'block';
+    } else {
+        newSchedule.style.display = 'none';
+    }
 })
